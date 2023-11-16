87041a3f8b5d39ab97bc599ad568f8a888269137
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2023-11-16 02:40:17 +0200

    Initial setup (#1)
    * chore: setup gitignore
    * chore: initialize empty swift package
    * chore: remove test target

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftyTestAssertions",
+    products: [
+        .library(
+            name: "SwiftyTestAssertions",
+            targets: ["SwiftyTestAssertions"]),
+    ],
+    targets: [
+        .target(name: "SwiftyTestAssertions"),
+    ]
+)
